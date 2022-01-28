0a8907ed56f59cc00761b529a1bbd11d289a9539
Claudia Scarbrough <rubyharrts@gmail.com>
2022-01-28 15:57:41 -0600

    login works

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,57 @@
+import axios from "axios";
 import React, { useState } from "react";
 import styled from "styled-components";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    credentials: {
+      username: "",
+      password: "",
+    },
+  });
   const [error, setError] = useState(null);
+  const { push } = useHistory();
+
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      credentials: {
+        ...user.credentials,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios
+      .post(`http://localhost:9000/api/login`, user.credentials)
+      .then((res) => {
+        localStorage.setItem("token", res.data.token);
+        localStorage.setItem("username", res.data.username);
+        push("/view");
+      })
+      .catch((err) => {
+        console.log(err);
+        setError("Invalid Login Information");
+      });
+  };
 
   return (
     <ComponentContainer>
       <ModalContainer>
         <h1>Welcome to Blogger Pro</h1>
         <h2>Please enter your account information.</h2>
-        <form>
+        <form onSubmit={handleLogin}>
           <Label htmlFor="username">
             Username
             <input
               type="text"
               name="username"
               id="username"
-              value=""
-              onChange=""
+              value={user.credentials.username}
+              onChange={handleChange}
             />
           </Label>
           <Label htmlFor="password">
@@ -26,8 +60,8 @@ const Login = () => {
               type="text"
               name="password"
               id="password"
-              value=""
-              onChange=""
+              value={user.credentials.password}
+              onChange={handleChange}
             />
           </Label>
           <button id="submit">Submit</button>
